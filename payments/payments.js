494fe78b1e8d2ae05f4b6e8602bc0e62c39814fe
Ajay <ajaydarez@gmail.com>
2018-05-14 19:59:54 -0700

    add payments request button

--- payments/payments.js
+++ payments/payments.js
@@ -55,3 +55,40 @@ form.addEventListener('submit', function(event) {
     }
   });
 });
+
+var paymentRequest = stripe.paymentRequest({
+  country: 'US',
+  currency: 'usd',
+  total: {
+    label: 'Demo total',
+    amount: 150,
+  },
+});
+
+var elements = stripe.elements();
+var prButton = elements.create('paymentRequestButton', {
+  paymentRequest: paymentRequest,
+});
+
+// Check the availability of the Payment Request API first.
+paymentRequest.canMakePayment().then(function(result) {
+  if (result) {
+    prButton.mount('#payment-request-button');
+  } else {
+    document.getElementById('payment-request-button').style.display = 'none';
+  }
+});
+
+paymentRequest.on('token', function(ev) {
+  // Send the token to your server to charge it!
+  console.log(JSON.stringify(ev.token))
+  
+});
+
+
+
+
+
+
+
+
